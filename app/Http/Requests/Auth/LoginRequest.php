48a463b9204ca560c3ae244057cb99c4afd70405
NguyenNam27 <nguyenngocnam00770@gmail.com>
2023-01-03 00:04:40 +0700

    2/1/2023

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests\Auth;
 
+use Dotenv\Exception\ValidationException;
+use Illuminate\Cache\RateLimiter;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
@@ -29,11 +31,21 @@ class LoginRequest extends FormRequest
             //
         ];
     }
+    public function throttleKey(){
+        return Str::lower($this->input('email')).'|'.$this->ip();
+    }
     public function authenticateToken() : string
     {
         if (! $token = Auth::attempt($this->only('email','password'))){
-            
+            RateLimiter::hit($this->throttleKey());
+
+            throw ValidationException::withMessages([
+               'email'=>__('auth.failed'),
+            ]);
+
         }
-        return ;
+        RateLimiter::clear($this->throttleKey());
+        return $token;
     }
+
 }
